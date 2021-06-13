6028443c926bc001973def556eb5adbe4b533c61
Sam <samara.amanda01@gmail.com>
2021-06-13 16:01:24 -0400

    commit tree 016856c4bcf80b0ab65e6a0d495f86c94112d29a

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+    switch( action.type ) {
+        case "ADD_FRIEND": 
+          return {friends: [...state.friends, action.friend] }
+        case "REMOVE_FRIEND" :
+          let friendsArray = [] 
+          state.friends.map( friend => {
+            if ( friend.id != action.id ) {
+              friendsArray.push(friend)
+            }
+          })
+          return {friends: friendsArray}
+        default: 
+          return {friends: state.friends}
+      }
 }
